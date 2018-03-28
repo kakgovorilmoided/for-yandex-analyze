f0ec1875d4132cce4d45205f500c7168e3fda907
kakgovorilmoided <belsalovephysics@gmail.com>
2018-03-28 03:11:12 +0300

    just trying

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
 for i in range(5):
     print('Kogo ebet vo shto ja odet')
-
+asdas
+das
+dasd
+ascii(das
+      d
+asd
+as
+da
+sd
+ad
+as
+delattr(asd
+        as
+delattr(asd
+        ascii(
+            
+        ))))
